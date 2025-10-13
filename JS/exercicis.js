dea6d95ec3cbb6ba05645ec2f08bb5a6be1594d5
nmassaa <nmassauab@gmail.com>
2025-10-13 19:37:45 +0200

    Create exercicis.js

---
+++ JS/exercicis.js
@@ -0,0 +1,28 @@
+/* Exercici 1
+let numero = Number(prompt("Introdueix un número:"));
+if (numero < 0) {console.log("El número és negatiu");} */
+/*EXERCICI 2
+let numero=Number(prompt("Escriu un nombre:"));
+if (numero % 2 ==0) {
+    console.log("El numero és parell")
+}else {console.log("El numero és senar ")}; */
+/*13.4
+let numero=Number(prompt("Introdueix un numero"));
+let numero2=Number(prompt("Introdueix un segon numero"));
+if (numero>numero2) {console.log(numero);
+
+}else {console.log(numero2)};*/
+/*13.5
+let num1=Number(prompt("Escriu un numero"))
+let num2=Number(prompt("Escriu un segon numero"))
+let num3=Number(prompt("Escriu un tercer numero"))
+let el_mes_gran;
+if (num1> num2 && num1>num3){
+    el_mes_gran=num1
+    console.log("El numero mes gran és",el_mes_gran)
+}else if (num2> num1 && num2>num3){
+    el_mes_gran=num2
+    console.log("El numero mes gran és",el_mes_gran)
+}else{
+    el_mes_gran=num3 
+    console.log("El numero mes gran és",el_mes_gran)}*/
